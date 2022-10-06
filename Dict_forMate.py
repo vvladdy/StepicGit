dd93ac4bef43b7cc4051b19a38cf637435c3a992
vvladdy <yablonvl@icloud.com>
2022-10-06 15:18:38 +0300

    Словари

--- Dict_forMate.py
+++ Dict_forMate.py
@@ -117,3 +117,120 @@ print(outdict)
 lists = zip(st_ids, st_names, st_grades)
 listoutdict = [{k[0]: {k[1]: k[2]}} for k in lists]
 print(listoutdict)
+
+d=dict([('foo', 100), ('bar', 200)])
+print('111111',d)
+d1={('foo', 100), ('bar', 200)}
+print('111111',d1)
+d2 = dict(foo=100, bar=200)
+print(d2)
+
+recep = {'hum': 209,
+         'biol': 1473}
+#recep.update(['scr', 131, 'math', 34])
+# recep.update({'scr', 131, 'math', 34})
+# recep.update('scr', 131, 'math', 34)
+print(recep)
+
+stud = {
+    'С1': [10, 75, 3, 67], 'R1': [1, 5, 8], 'Sultan': [1, 5, 8, 8,3],
+    'Ivan': [170, 55, 5]
+}
+
+# stud = {k: [v for k, v in stud.items() if i < 5 for i in v]}
+# print(stud)
+
+stud1 = {}
+for k, v in stud.items():
+    for i in v:
+        # print(i)
+        if i < 5:
+            print(i)
+            stud1[k] = stud1.setdefault(k, i)
+stud = stud1
+print(stud)
+
+
+# results = {k: v for k, v in students.items() if (v[0] < 180 and v[1] < 70)}
+# print(results)
+
+stword = 'FRESHENER'
+
+stdict = {
+    1: ['A', 'E', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10:['Q', 'Z']
+}
+stworddict = {}
+for i in stword:
+    stworddict[i] = stworddict.setdefault(i, 0) + 1
+print('stworddict', stworddict)
+count = 0
+for k_i, v_i in stdict.items():
+    for k_j, v_j in stworddict.items():
+        for el in v_i:
+            if k_j == el:
+                count += k_i*v_j
+print('count', count)
+
+# задача. Вывести порядковый номер вхождения данного слова в предложение
+sent = 'ppp hhh p p p'.split(' ')
+newlist = []
+newdict = {}
+
+for el in sent:
+    if el in newdict:
+        print(f'{el}_{newdict[el]}', end = ' ')
+    else:
+        print(f'{el}_1', end=' ')
+    newdict[el] = newdict.get(el, 1) + 1
+
+print()
+listfordict = ([{'a': 1, 'b': 2}, {'b': 10, 'c': 100},
+                {'a': 1, 'b': 17, 'c': 50},
+                {'a': 5, 'd': 777}])
+dictfordict = {}
+otherdict = {}
+for i in listfordict:
+    for k, v in i.items():
+        dictfordict[k] = dictfordict.get(k, []) + [v]
+        otherdict.update(i)
+# print(dictfordict)
+# print(otherdict)
+# # тоже самое
+# super_dict1 = {key:val for d in listfordict for key,val in d.items()}
+# print(super_dict1)
+
+# нужно было объединить словари из списка
+
+super_dict = {}
+for k in (k for d in listfordict for k in d):
+    super_dict[k] = {d[k] for d in listfordict if k in d}
+print(super_dict)
+
+def print_products(*args):
+    count = 0
+    tab = []
+    for i in args:
+        if type(i) == str and i != '':
+            tab.append(i)
+            if len(tab) == 0:
+                print('Нет продуктов')
+            else:
+                count += 1
+                print (f'{count}) {i}')
+
+
+print_products('Бананы', [1, 2], ('Stepic',), 'Яблоки', '', 'Макароны',
+                    5, True)
+print_products('')
+
+def my_kwards_func(**kwargs):
+    for k, v in sorted(kwargs.items()):
+        print(f'{k}: {v}')
+
+my_kwards_func(job='TEacher', language='Python')
